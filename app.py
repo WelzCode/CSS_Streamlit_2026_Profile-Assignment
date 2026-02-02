5596a8ad4a70a3c1251099520e79719f98b4b4a9
WelzCode <wellzchirwa22@gmail.com>
2026-02-02 02:40:07 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 31 19:26:09 2026
+
+@author: wellz
+"""
+
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_title="Researcher Profile and STEM Explorer",
+    layout="wide"
+)
+
+#st.sidebar.title("Navigationg")
+
+st.title("Welcome")
+st.write(
+    """
+    This application is a short preview for the CSS_2026 profile deployment, To view the modifications pease visit the Profile tab and thank you.
+    """
+)
